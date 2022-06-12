a47a80de8d348e52712f17a74099b66bf70abeed
NingBo Wang <2536935847@qq.com>
2022-06-12 13:19:14 +0800

    Add script

--- script/dev_init.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-echo "building chain spec..."
-./target/release/diora build-spec --chain dev --raw --disable-default-bootnode > dev-testnet.json
-echo "building genesis..."
-./target/release/diora export-genesis-state --chain dev > dev-genesis
-echo "building wasm..."
-./target/release/diora export-genesis-wasm --chain dev > dev-wasm
-echo "complate!"
