acbef099b0303cf8eff36889553470140e365f72
Jay <dunnpoker98@gmail.com>
2022-06-17 08:45:27 +1000

    Update chain_spec.rs

--- node/src/chain_spec.rs
+++ node/src/chain_spec.rs
@@ -1,10 +1,11 @@
 use cumulus_primitives_core::ParaId;
-use diora_runtime::{AccountId, EVMConfig, EthereumConfig, GenesisConfig, NimbusId, Signature};
+use diora_runtime::{AccountId, BalancesConfig, EVMConfig, EthereumConfig, GenesisConfig, NimbusId, Signature};
 use sc_chain_spec::{ChainSpecExtension, ChainSpecGroup};
 use sc_service::ChainType;
 use serde::{Deserialize, Serialize};
-use sp_core::{sr25519, Pair, Public};
+use sp_core::{sr25519, Pair, Public, H160, U256};
 use sp_runtime::traits::{IdentifyAccount, Verify};
+use std::{collections::BTreeMap, str::FromStr};
 
 /// Specialized `ChainSpec` for the normal parachain runtime.
 pub type ChainSpec =
@@ -66,24 +67,26 @@ pub fn development_config() -> ChainSpec {
 		ChainType::Development,
 		move || {
 			testnet_genesis(
-				// Alith is Sudo
-				accounts[0],
-				// Council members: Baltathar, Charleth and Dorothy
-				vec![accounts[1], accounts[2], accounts[3]],
-				// Tech comitee members: Alith and Baltathar
-				vec![accounts[0], accounts[1]],
-				// Collator Candidate: Alice -> Alith
+				// initial collators.
 				vec![(
-					accounts[0],
-					get_from_seed::<NimbusId>("Alice"),
-					1_000 * UNIT,
+					get_account_id_from_seed::<sr25519::Public>("Alice"),
+					get_collator_keys_from_seed("Alice"),
 				)],
-				// Delegations
-				vec![],
-				accounts.clone(),
-				3_000_000 * UNIT,
-				Default::default(), // para_id
-				201,               //ChainId
+				vec![
+					get_account_id_from_seed::<sr25519::Public>("Alice"),
+					get_account_id_from_seed::<sr25519::Public>("Bob"),
+					get_account_id_from_seed::<sr25519::Public>("Charlie"),
+					get_account_id_from_seed::<sr25519::Public>("Dave"),
+					get_account_id_from_seed::<sr25519::Public>("Eve"),
+					get_account_id_from_seed::<sr25519::Public>("Ferdie"),
+					get_account_id_from_seed::<sr25519::Public>("Alice//stash"),
+					get_account_id_from_seed::<sr25519::Public>("Bob//stash"),
+					get_account_id_from_seed::<sr25519::Public>("Charlie//stash"),
+					get_account_id_from_seed::<sr25519::Public>("Dave//stash"),
+					get_account_id_from_seed::<sr25519::Public>("Eve//stash"),
+					get_account_id_from_seed::<sr25519::Public>("Ferdie//stash"),
+				],
+				1000.into(),
 			)
 		},
 		vec![],
@@ -177,6 +180,41 @@ fn testnet_genesis(
 				.map(|k| (k, 1 << 60))
 				.collect(),
 		},
+		sudo: SudoConfig {
+			// Assign network admin rights.
+			key: Some(root_key),
+		},
+		evm: EVMConfig {
+			accounts: {
+				let mut map = BTreeMap::new();
+				map.insert(
+					// H160 address of Alice dev account
+					// Derived from SS58 (42 prefix) address
+					// SS58: 5GrwvaEF5zXb26Fz9rcQpDWS57CtERHpNehXCPcNoHGKutQY
+					// hex: 0xd43593c715fdd31c61141abd04a99fd6822c8558854ccde39a5684e7a56da27d
+					// Using the full hex key, truncating to the first 20 bytes (the first 40 hex chars)
+					H160::from_str("d43593c715fdd31c61141abd04a99fd6822c8558")
+						.expect("internal H160 is valid; qed"),
+					fp_evm::GenesisAccount {
+						balance: U256::from_str("0xffffffffffffffffffffffffffffffff")
+							.expect("internal U256 is valid; qed"),
+						code: Default::default(),
+						nonce: Default::default(),
+						storage: Default::default(),
+					},
+				);
+				map.insert(
+					// H160 address of CI test runner account
+					H160::from_str("6be02d1d3665660d22ff9624b7be0551ee1ac91b")
+						.expect("internal H160 is valid; qed"),
+					fp_evm::GenesisAccount {
+						balance: U256::from_str("0xffffffffffffffffffffffffffffffff")
+							.expect("internal U256 is valid; qed"),
+						code: Default::default(),
+						nonce: Default::default(),
+						storage: Default::default(),
+					},
+				);
 		parachain_info: diora_runtime::ParachainInfoConfig { parachain_id: id },
 		author_filter: diora_runtime::AuthorFilterConfig {
 			eligible_count: diora_runtime::EligibilityValue::default(),
@@ -185,7 +223,6 @@ fn testnet_genesis(
 			mapping: authorities,
 		},
 		parachain_system: Default::default(),
-		evm: Default::default(),
 		ethereum: Default::default(),
 		base_fee: Default::default(),
 	}
