a9ba76d46561777ad0b0749abc4c03fce060f8a9
Jay <dunnpoker98@gmail.com>
2022-07-09 11:29:04 +1000

    Update pallet_account_set.rs

--- runtime/src/pallet_account_set.rs
+++ runtime/src/pallet_account_set.rs
@@ -9,210 +9,83 @@
 //! Moonbeam's Parachain Staking - https://github.com/PureStake/moonbeam/tree/master/pallets/parachain-staking
 //! Recipe for AccountSet, VecSet, and MapSet
 
-#![cfg_attr(not(feature = "std"), no_std)]
+use frame_support::pallet;
 
-use parity_scale_codec::{Decode, Encode, MaxEncodedLen};
-use scale_info::TypeInfo;
-use sha3::{Digest, Keccak256};
-use sp_core::{ecdsa, H160, H256};
+pub use pallet::*;
 
-#[cfg(feature = "std")]
-pub use serde::{de::DeserializeOwned, Deserialize, Serialize};
+#[pallet]
+pub mod pallet {
 
-//TODO Maybe this should be upstreamed into Frontier (And renamed accordingly) so that it can
-// be used in palletEVM as well. It may also need more traits such as AsRef, AsMut, etc like
-// AccountId32 has.
+    use frame_support::pallet_prelude::*;
+    #[cfg(feature = "std")]
+    use log::warn;
+    use nimbus_primitives::{AccountLookup, CanAuthor, NimbusId};
+    use sp_std::vec::Vec;
 
-/// The account type to be used in Moonbeam. It is a wrapper for 20 fixed bytes. We prefer to use
-/// a dedicated type to prevent using arbitrary 20 byte arrays were AccountIds are expected. With
-/// the introduction of the `scale-info` crate this benefit extends even to non-Rust tools like
-/// Polkadot JS.
+    /// The Account Set pallet
+    #[pallet::pallet]
+    #[pallet::without_storage_info]
+    pub struct Pallet<T>(PhantomData<T>);
 
-#[derive(
-	Eq, PartialEq, Copy, Clone, Encode, Decode, TypeInfo, MaxEncodedLen, Default, PartialOrd, Ord,
-)]
-pub struct AccountId20(pub [u8; 20]);
+    /// Configuration trait of this pallet.
+    #[pallet::config]
+    pub trait Config: frame_system::Config {}
 
-#[cfg(feature = "std")]
-impl_serde::impl_fixed_hash_serde!(AccountId20, 20);
+    /// The set of accounts that is stored in this pallet.
+    #[pallet::storage]
+    pub type StoredAccounts<T: Config> = StorageValue<_, Vec<T::AccountId>, ValueQuery>;
 
-#[cfg(feature = "std")]
-impl std::fmt::Display for AccountId20 {
-	//TODO This is a pretty quck-n-dirty implementation. Perhaps we should add
-	// checksum casing here? I bet there is a crate for that.
-	// Maybe this one https://github.com/miguelmota/rust-eth-checksum
-	fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-		write!(f, "{:?}", self.0)
-	}
-}
+    impl<T: Config> Get<Vec<T::AccountId>> for Pallet<T> {
+        fn get() -> Vec<T::AccountId> {
+            StoredAccounts::<T>::get()
+        }
+    }
 
-impl core::fmt::Debug for AccountId20 {
-	fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
-		write!(f, "{:?}", H160(self.0))
-	}
-}
+    #[pallet::storage]
+    #[pallet::getter(fn account_id_of)]
+    /// A mapping from the AuthorIds used in the consensus layer
+    /// to the AccountIds runtime.
+    type Mapping<T: Config> = StorageMap<_, Twox64Concat, NimbusId, T::AccountId, OptionQuery>;
 
-impl From<[u8; 20]> for AccountId20 {
-	fn from(bytes: [u8; 20]) -> Self {
-		Self(bytes)
-	}
-}
+    #[pallet::genesis_config]
+    /// Genesis config for author mapping pallet
+    pub struct GenesisConfig<T: Config> {
+        /// The associations that should exist at chain genesis
+        pub mapping: Vec<(T::AccountId, NimbusId)>,
+    }
 
-impl Into<[u8; 20]> for AccountId20 {
-	fn into(self) -> [u8; 20] {
-		self.0
-	}
-}
+    #[cfg(feature = "std")]
+    impl<T: Config> Default for GenesisConfig<T> {
+        fn default() -> Self {
+            Self { mapping: vec![] }
+        }
+    }
 
-impl From<H160> for AccountId20 {
-	fn from(h160: H160) -> Self {
-		Self(h160.0)
-	}
-}
+    #[pallet::genesis_build]
+    impl<T: Config> GenesisBuild<T> for GenesisConfig<T> {
+        fn build(&self) {
+            if self.mapping.is_empty() {
+                warn!(target: "account-set", "No mappings at genesis. Your chain will have no valid authors.");
+            }
+            for (account_id, author_id) in &self.mapping {
+                Mapping::<T>::insert(author_id, account_id);
+                StoredAccounts::<T>::append(account_id);
+            }
+        }
+    }
 
-impl Into<H160> for AccountId20 {
-	fn into(self) -> H160 {
-		H160(self.0)
-	}
-}
+    /// This pallet is compatible with nimbus's author filtering system. Any account stored in this pallet
+    /// is a valid author. Notice that this implementation does not have an inner filter, so it
+    /// can only be the beginning of the nimbus filter pipeline.
+    impl<T: Config> CanAuthor<T::AccountId> for Pallet<T> {
+        fn can_author(author: &T::AccountId, _slot: &u32) -> bool {
+            StoredAccounts::<T>::get().contains(author)
+        }
+    }
 
-#[cfg(feature = "std")]
-impl std::str::FromStr for AccountId20 {
-	type Err = &'static str;
-	fn from_str(input: &str) -> Result<Self, Self::Err> {
-		H160::from_str(input)
-			.map(Into::into)
-			.map_err(|_| "invalid hex address.")
-	}
-}
-
-#[cfg_attr(feature = "std", derive(serde::Serialize, serde::Deserialize))]
-#[derive(Eq, PartialEq, Clone, Encode, Decode, sp_core::RuntimeDebug, TypeInfo)]
-pub struct EthereumSignature(ecdsa::Signature);
-
-impl From<ecdsa::Signature> for EthereumSignature {
-	fn from(x: ecdsa::Signature) -> Self {
-		EthereumSignature(x)
-	}
-}
-
-impl sp_runtime::traits::Verify for EthereumSignature {
-	type Signer = EthereumSigner;
-	fn verify<L: sp_runtime::traits::Lazy<[u8]>>(&self, mut msg: L, signer: &AccountId20) -> bool {
-		let mut m = [0u8; 32];
-		m.copy_from_slice(Keccak256::digest(msg.get()).as_slice());
-		match sp_io::crypto::secp256k1_ecdsa_recover(self.0.as_ref(), &m) {
-			Ok(pubkey) => {
-				// TODO This conversion could use a comment. Why H256 first, then H160?
-				// TODO actually, there is probably just a better way to go from Keccak digest.
-				AccountId20(H160::from(H256::from_slice(Keccak256::digest(&pubkey).as_slice())).0)
-					== *signer
-			}
-			Err(sp_io::EcdsaVerifyError::BadRS) => {
-				log::error!(target: "evm", "Error recovering: Incorrect value of R or S");
-				false
-			}
-			Err(sp_io::EcdsaVerifyError::BadV) => {
-				log::error!(target: "evm", "Error recovering: Incorrect value of V");
-				false
-			}
-			Err(sp_io::EcdsaVerifyError::BadSignature) => {
-				log::error!(target: "evm", "Error recovering: Invalid signature");
-				false
-			}
-		}
-	}
-}
-
-/// Public key for an Ethereum / Moonbeam compatible account
-#[derive(Eq, PartialEq, Ord, PartialOrd, Clone, Encode, Decode, sp_core::RuntimeDebug)]
-#[cfg_attr(feature = "std", derive(serde::Serialize, serde::Deserialize))]
-pub struct EthereumSigner([u8; 20]);
-
-impl sp_runtime::traits::IdentifyAccount for EthereumSigner {
-	type AccountId = AccountId20;
-	fn into_account(self) -> AccountId20 {
-		AccountId20(self.0)
-	}
-}
-
-impl From<[u8; 20]> for EthereumSigner {
-	fn from(x: [u8; 20]) -> Self {
-		EthereumSigner(x)
-	}
-}
-
-impl From<ecdsa::Public> for EthereumSigner {
-	fn from(x: ecdsa::Public) -> Self {
-		let decompressed = libsecp256k1::PublicKey::parse_slice(
-			&x.0,
-			Some(libsecp256k1::PublicKeyFormat::Compressed),
-		)
-		.expect("Wrong compressed public key provided")
-		.serialize();
-		let mut m = [0u8; 64];
-		m.copy_from_slice(&decompressed[1..65]);
-		let account = H160::from(H256::from_slice(Keccak256::digest(&m).as_slice()));
-		EthereumSigner(account.into())
-	}
-}
-
-impl From<libsecp256k1::PublicKey> for EthereumSigner {
-	fn from(x: libsecp256k1::PublicKey) -> Self {
-		let mut m = [0u8; 64];
-		m.copy_from_slice(&x.serialize()[1..65]);
-		let account = H160::from(H256::from_slice(Keccak256::digest(&m).as_slice()));
-		EthereumSigner(account.into())
-	}
-}
-
-#[cfg(feature = "std")]
-impl std::fmt::Display for EthereumSigner {
-	fn fmt(&self, fmt: &mut std::fmt::Formatter) -> std::fmt::Result {
-		write!(fmt, "ethereum signature: {:?}", H160::from_slice(&self.0))
-	}
-}
-
-#[cfg(test)]
-mod tests {
-	use super::*;
-	use sp_core::{ecdsa, Pair};
-	use sp_runtime::traits::IdentifyAccount;
-
-	#[test]
-	fn test_account_derivation_1() {
-		// Test from https://asecuritysite.com/encryption/ethadd
-		let secret_key =
-			hex::decode("502f97299c472b88754accd412b7c9a6062ef3186fba0c0388365e1edec24875")
-				.unwrap();
-		let mut expected_hex_account = [0u8; 20];
-		hex::decode_to_slice(
-			"976f8456e4e2034179b284a23c0e0c8f6d3da50c",
-			&mut expected_hex_account,
-		)
-		.expect("example data is 20 bytes of valid hex");
-
-		let public_key = ecdsa::Pair::from_seed_slice(&secret_key).unwrap().public();
-		let account: EthereumSigner = public_key.into();
-		let expected_account = AccountId20::from(expected_hex_account);
-		assert_eq!(account.into_account(), expected_account);
-	}
-	#[test]
-	fn test_account_derivation_2() {
-		// Test from https://asecuritysite.com/encryption/ethadd
-		let secret_key =
-			hex::decode("0f02ba4d7f83e59eaa32eae9c3c4d99b68ce76decade21cdab7ecce8f4aef81a")
-				.unwrap();
-		let mut expected_hex_account = [0u8; 20];
-		hex::decode_to_slice(
-			"420e9f260b40af7e49440cead3069f8e82a5230f",
-			&mut expected_hex_account,
-		)
-		.expect("example data is 20 bytes of valid hex");
-
-		let public_key = ecdsa::Pair::from_seed_slice(&secret_key).unwrap().public();
-		let account: EthereumSigner = public_key.into();
-		let expected_account = AccountId20::from(expected_hex_account);
-		assert_eq!(account.into_account(), expected_account);
-	}
+    impl<T: Config> AccountLookup<T::AccountId> for Pallet<T> {
+        fn lookup_account(author: &NimbusId) -> Option<T::AccountId> {
+            Mapping::<T>::get(&author)
+        }
+    }
 }
