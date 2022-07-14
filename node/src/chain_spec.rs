792e8d64a99e1ca8957914ebe93196668f7d0d88
Jay <dunnpoker98@gmail.com>
2022-07-15 09:49:45 +1000

    Update chain_spec.rs

--- node/src/chain_spec.rs
+++ node/src/chain_spec.rs
@@ -6,11 +6,11 @@ use diora_runtime::{
     ParachainInfoConfig, ParachainStakingConfig, Perbill, PotentialAuthorSetConfig, Range,
     Signature, SudoConfig, SystemConfig, WASM_BINARY,
 };
-use hex_literal::hex;
+
 use sc_chain_spec::{ChainSpecExtension, ChainSpecGroup};
 use sc_service::ChainType;
 use serde::{Deserialize, Serialize};
-use sp_core::{sr25519, Pair, Public, H160, U256};
+use sp_core::{sr25519, Pair, Public};
 use sp_core::crypto::AccountId32;
 use sp_runtime::traits::{IdentifyAccount, Verify};
 
