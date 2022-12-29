e7f64498d5f0f2ce690dcc14f8f4c7824b685536
Jay <dunnpoker98@gmail.com>
2022-12-30 07:30:04 +1100

    Update remix.sh

--- script/remix.sh
+++ script/remix.sh
@@ -1,4 +1,5 @@
 apt-get -y install nodejs
 apt-get -y install npm
+apt-get -y install python3-pip
 npm install -g @remix-project/remixd
 remixd -s ./contracts/ --remix-ide http://remix.ethereum.org/
