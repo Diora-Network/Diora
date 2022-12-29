27c71e560961cfcb7bb3f768e935d480b6801ef6
Jay <dunnpoker98@gmail.com>
2022-12-30 09:40:31 +1100

    Update start2.sh

--- script/start2.sh
+++ script/start2.sh
@@ -2,4 +2,4 @@
 mkdir data > /dev/null 2>&1 &
 
 # parachain
-nohup ./diora -d ./data/diora --bob --force-authoring --collator --discover-local --rpc-cors=all --ws-port 9944 --rpc-port 9933 --port 40041 --chain raw-diora-chainspec.json -llog=info -lruntime=debug,evm=trace --  raw-diora-chainspec.json --discover-local --port 40042 > data/log.2022 2>&1 &
+nohup ./diora -d ./target/release/diora --bob --force-authoring --collator --discover-local --rpc-cors=all --ws-port 9944 --rpc-port 9933 --port 40041 --chain raw-diora-chainspec.json -llog=info -lruntime=debug,evm=trace --  raw-diora-chainspec.json --discover-local --port 40042 > data/log.2022 2>&1 &
