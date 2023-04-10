95476ddeb1cb1b44fefe4638fa3dca1a01d639af
Jay <dunnpoker98@gmail.com>
2023-04-10 19:37:28 +1000

    Update generate_key.sh

--- script/generate_key.sh
+++ script/generate_key.sh
@@ -5,6 +5,6 @@
 # This script will print the info to stdout in a form which is slightly easier to be
 # copied and pasted to the Rust source file.
 
-./diora key insert --chain diora_rococo --keystore-path ./keystore1 --suri "${SECRET1}" --scheme Sr25519 --key-type nmbs
-./diora key insert --chain diora_rococo --keystore-path ./keystore2 --suri "${SECRET2}" --scheme Sr25519 --key-type nmbs
-./diora key insert --chain diora_rococo --keystore-path ./keystore3 --suri "${SECRET3}" --scheme Sr25519 --key-type nmbs
+./Diora key insert --chain diora_rococo --keystore-path ./keystore1 --suri "${SECRET1}" --scheme Sr25519 --key-type nmbs
+./Diora key insert --chain diora_rococo --keystore-path ./keystore2 --suri "${SECRET2}" --scheme Sr25519 --key-type nmbs
+./Diora key insert --chain diora_rococo --keystore-path ./keystore3 --suri "${SECRET3}" --scheme Sr25519 --key-type nmbs
